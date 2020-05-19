cd8fae3add5cb07014b2fd66219d3922a5fb45b5
Юрий <yydevlyashov@gmail.com>
2020-05-19 19:33:22 +0400

    header player fix

--- src/blocks/header/header.js
+++ src/blocks/header/header.js
@@ -3,6 +3,7 @@ class Player {
         this.sound = new Audio();
         this.sound.src = '/src/blocks/header/sounds/soundtrack.mp3';
         this.soundStatus = document.querySelector('.sound-icon-off');
+        this.equalizer = document.querySelector('.equalizer');
         this.switch = true;
         this.event();
     }
@@ -19,12 +20,14 @@ class Player {
     }
 
     play() {
+        this.equalizer.style.display = "block";
         this.soundStatus.className ='sound-icon-on';
         this.sound.play();
         this.switch = false;
     }
 
     pause() {
+        this.equalizer.style.display = "none";
         this.soundStatus.className ='sound-icon-off';
         this.sound.pause();
         this.switch = true;
