a8444173d3f0783271055d5cb7fba2459ae59442
Юрий Девляшов <yury.devlyashov@migogroup.ru>
2020-07-20 11:25:52 +0400

    block video+

--- src/blocks/gallery/slider/slider.js
+++ src/blocks/gallery/slider/slider.js
@@ -2,28 +2,25 @@ class Slider {
     constructor () {
         this.button = document.querySelector('.slider-switch__btn');
         this.imgCollections = [...document.querySelectorAll('.slider-img')];
-
-        this.imgCollections[0].style.display = "block";
+        this.sliderContainer = document.querySelector('.slider__content-container');
+        this.widthImg = -350;
+        this.positionImg = 0;
+        // this.count = 1;
 
         this.btnClick();
     }
 
     btnClick() {
       this.button.addEventListener('click', () => {  
-        this.imgCollections[1].style.marginLeft = '800px';
+     
+        this.positionImg +=  this.widthImg;
+        console.log( this.positionImg);
+
+        this.sliderContainer.style.marginLeft = this.positionImg + 'px'; 
+        this.positionImg = this.positionImg;
+        
       });
     }
-    // btnClick() {
-    //     this.button.addEventListener('click', () => {  
-    //       this.imgCollections.forEach(element => {
-    //         element.style.display = "block" ? element.style.display = "none" : false;
-    //       });
-    //       this.arrItem = this.imgCollections.pop();
-    //       this.arrItem.style.display = "block";
-    //       this.imgCollections.unshift(this.arrItem);
-    //       });         
-    // }
-
 
 }
 new Slider();
